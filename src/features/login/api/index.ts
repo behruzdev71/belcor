6077ac4ceb9d568eefbe7e4b7da882f257db234a
Behruz <behruz@bictory.io>
2024-02-24 18:51:26 +0500

    [8] fix (loginApi)

--- src/features/login/api/index.ts
+++ src/features/login/api/index.ts
@@ -1,10 +1,8 @@
-import { IUser } from '@/entities/user/index.ts'
-
 import clientApi from '@/shared/api/base-api.ts'
 
 import { ILogin } from '../model/types.ts'
 
-export const loginApi = async (data: ILogin): Promise<IUser> => {
-  const response = await clientApi.post<IUser>('/auth/login',  data)
+export const loginApi = async (data: ILogin): Promise<{token: string}> => {
+  const response = await clientApi.post<{token: string}>('/auth/login',  data)
   return response.data
 }
